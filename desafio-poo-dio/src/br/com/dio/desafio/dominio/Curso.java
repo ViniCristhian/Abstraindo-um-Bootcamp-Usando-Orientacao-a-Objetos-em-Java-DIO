4f1e515888208979114e3fa203ac86730cbebd30
ViniciusCris <143042392+Vini14cius09@users.noreply.github.com>
2024-06-02 22:24:11 -0300

    BootCamp e Devs, criados.

--- desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,27 +1,15 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
-    
+
+    @Override
+    public double culcularXp() {
+        return XP_PADRAO + cargaHoraria;
+    }
+
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -34,7 +22,7 @@ public class Curso {
 
     @Override
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
     }
 
     
